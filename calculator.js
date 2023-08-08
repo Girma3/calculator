d1bb3dbcc446ca2c364b1cb437ff00ac885418a4
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2023-08-08 16:50:52 +0300

    Event listener added to the buttons

--- calculator.js
+++ calculator.js
@@ -1,8 +1,9 @@
 //functions to do basic maths
 const sum = (a, b)=> {return a + b};
 const substract = (a, b)=> {return a - b};
-const multiplication = (a, b)=> {return a * b};
+const multiply = (a, b)=> {return a * b};
 const divide = (a,b )=> {return a / b};
+
 //functions to use percent
 const percent = (number)=> {return mnumber / 100}   
 
@@ -10,3 +11,66 @@ const percent = (number)=> {return mnumber / 100}
 let firstNum;
 let secondNum;
 let currentOperator;
+
+//function that take 2 numbers and operator then call one of the above functions
+function operate(num1,operator,num2){
+    let result;
+        if(operator == "+"){
+            result = sum(num1, num2)
+        }
+        else if(operator == "-"){
+            result = substract(num1,num2)
+        }
+        else if(operator == "x"){
+            result = multiply(num1,num2)
+        }
+        else if(operator == "÷"){
+            result = divide(num1,num2)
+        }
+    return result
+}
+
+//function to populate display
+const populate = (item)=>{return  this.textcontent += item}
+
+const numbers = document.querySelectorAll("[data-number]"); 
+const operators = document.querySelectorAll("[data-operator]");
+const displayCurrent = document.getElementById("display-current");
+const displayNext = document.getElementById("display-next");
+const equal =document.getElementById("equals");
+const clearAll = document.getElementById("clear");
+const backSpace = document.getElementById("delete");
+const percents = document.getElementById("percent"); 
+
+//add eventListeners to the buttons to get user input
+numbers.forEach(number=>{
+    number.addEventListener('click',()=>{
+        number.textContent
+        
+    });
+
+});
+
+operators.forEach(oprator=>{
+    oprator.addEventListener('click',() => {
+
+    });
+});
+
+equal.addEventListener('click',() => {
+console.log("hoooo")
+});
+
+clearAll.addEventListener('click',() => {
+
+});
+
+backSpace.addEventListener('click',() => {
+
+});
+
+percents.addEventListener('click',() => {
+
+});
+
+
