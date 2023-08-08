18fbb03ae6ee87ae2cf1988f1154eb89c4d7442d
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2023-08-08 16:01:19 +0300

    functions created to do basic operation on two numbers

---
+++ calculator.js
@@ -0,0 +1,12 @@
+//functions to do basic maths
+const sum = (a, b)=> {return a + b};
+const substract = (a, b)=> {return a - b};
+const multiplication = (a, b)=> {return a * b};
+const divide = (a,b )=> {return a / b};
+//functions to use percent
+const percent = (number)=> {return mnumber / 100}   
+
+//create 3 variables to calculate
+let firstNum;
+let secondNum;
+let currentOperator;
